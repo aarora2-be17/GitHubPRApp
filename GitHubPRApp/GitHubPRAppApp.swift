47530f6a2e50fd14919d9d16e00c0d0191f61056
Abhishek Arora <arorabhs@amazon.com>
2022-02-20 15:48:22 +0530

    Initial Commit

---
+++ GitHubPRApp/GitHubPRAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubPRAppApp.swift
+//  GitHubPRApp
+//
+//  Created by Arora, Abhishek on 20/02/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHubPRAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
